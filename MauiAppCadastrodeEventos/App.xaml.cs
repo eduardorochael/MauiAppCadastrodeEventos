7bbd278748292b800ee1312b05478cbaa01b2b1f
EDUARDO SILVA <eduardo.silva1305@etec.sp.gov.br>
2024-11-12 13:43:50 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppCadastrodeEventos/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiAppCadastrodeEventos
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
